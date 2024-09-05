538ab19e95047038865883a6621f8f73a27c01fe
Harish Raju <harisharaju1@gmail.com>
2024-09-05 14:42:52 +0530

    Navbar and Sidebar

---
+++ src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Menu } from "lucide-react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* TOP LOGO */}
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div className="">logo</div>
+        <h1 className="font-extrabold text-2xl">DJEWEL</h1>
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+      {/* LINKS */}
+      <div className="flex-grow mt-8">{/* links here */}</div>
+      {/* FOOTER */}
+      <div className="">
+        <p className="text-center text-xs text-gray-500">&copy; 2024 DJEWEL</p>
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
